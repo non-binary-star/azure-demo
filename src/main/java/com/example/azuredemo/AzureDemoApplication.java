8f4268f231e61409a7899f782bf39c54000c9349
Sveatoslav <sveat.kovas01@gmail.com>
2023-05-24 12:40:34 +0300

    Enable event hubs

--- src/main/java/com/example/azuredemo/AzureDemoApplication.java
+++ src/main/java/com/example/azuredemo/AzureDemoApplication.java
@@ -1,13 +1,30 @@
 package com.example.azuredemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class AzureDemoApplication {
+public class AzureDemoApplication implements CommandLineRunner {
+
+    @Value("${spring.cloud.azure.eventhubs.namespace}")
+    private String namespace;
+
+    @Value("${spring.cloud.azure.eventhubs.enabled}")
+    private Boolean enabled;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     public static void main(String[] args) {
         SpringApplication.run(AzureDemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println();
+    }
 }
