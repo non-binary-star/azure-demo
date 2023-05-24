8f927cf48b12d7640da59461017402b520fbc337
Sveatoslav <sveat.kovas01@gmail.com>
2023-05-24 13:57:56 +0300

    Add applicationinsights-runtime-attach

--- src/main/java/com/example/azuredemo/AzureDemoApplication.java
+++ src/main/java/com/example/azuredemo/AzureDemoApplication.java
@@ -1,30 +1,14 @@
 package com.example.azuredemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class AzureDemoApplication implements CommandLineRunner {
-
-    @Value("${spring.cloud.azure.eventhubs.namespace}")
-    private String namespace;
-
-    @Value("${spring.cloud.azure.eventhubs.enabled}")
-    private Boolean enabled;
-
-    @Autowired
-    private ApplicationContext applicationContext;
+public class AzureDemoApplication {
 
     public static void main(String[] args) {
+        ApplicationInsights.attach();
         SpringApplication.run(AzureDemoApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println();
     }
 }
